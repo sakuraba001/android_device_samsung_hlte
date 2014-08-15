3641e8d060fb6c393dd90acd6d60e6a7b9761c3d
sakuraba <sakura.bar.com@gmail.com>
2014-08-15 15:01:16 +0900

    typo

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #set -e
-export DEVICE=hlte
+export DEVICE=js01lte
 export VENDOR=samsung
 
 if [ $# -eq 0 ]; then
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -110,7 +110,7 @@ EOF
 
 LOCAL_PATH := \$(call my-dir)
 
-ifneq (\$(filter hlte,\$(TARGET_DEVICE)),)
+ifneq (\$(filter js01lte,\$(TARGET_DEVICE)),)
 
 include \$(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
